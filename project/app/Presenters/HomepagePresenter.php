4d3e9f3793e5ea17ab753dfd8c961b67543bc5eb
Jan Pospisil <me@jan-pospisil.cz>
2019-08-07 13:59:54 +0200

    Added form userform

--- project/app/Presenters/HomepagePresenter.php
+++ project/app/Presenters/HomepagePresenter.php
@@ -19,6 +19,22 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter {
 		$this->template->users = $this->context->table('user');
 	}
 
+	/**
+	 * @return \Nette\Application\UI\Control
+	 */
+	protected function createComponentUserForm(){
+		$form = new Nette\Application\UI\Form();
+		$form->addText('name', 'Name')->setRequired();
+		$form->addText('surname', 'Surname')->setRequired();
+		$form->addSubmit('formsubmit', 'Send');
+		$form->onSuccess[] = [$this, 'formSubmitted'];
+		return $form;
+	}
+
+	public function formSubmitted(Nette\Application\UI\Form $form, Nette\Utils\ArrayHash $value){
+		dumpe($value);
+	}
+
 }
 
 
